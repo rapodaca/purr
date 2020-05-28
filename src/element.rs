7a11ced01ef93cacbdf21b59311f161d2703ac60
Richard Apodaca <rich.apodaca@gmail.com>
2020-05-27 19:58:10 -0700

    Adds missing elements.

--- src/element.rs
+++ src/element.rs
@@ -1,11 +1,18 @@
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum Element {
-    H, He,
-    Li, Be, B, C, N, O, F, Ne,
-    Na, Mg, Al, Si, P, S, Cl, Ar,
-    K, Ca, Sc, Ti, V, Cr, Mn, Fe, Co, Ni, Cu, Zn, Ga, Ge, As, Se, Br, Kr,
-    Rb, Sr, Ce, Pr, Nd, Pm, Sm, Eu, Gd, Tb, Dy, Ho, Er, Tm, Yb, Lu,
-    Y, Zr, Nb, Mo, Tc, Ru, Rh, Pd, Ag, Cd, In, Sn, Sb, Te, I, Xe
+//  0   1   2   3   4   5   6   7   8   9
+        H,  He, Li, Be, B,  C,  N,  O,  F,  // 0
+    Ne, Na, Mg, Al, Si, P,  S,  Cl, Ar, K,  // 1
+    Ca, Sc, Ti, V,  Cr, Mn, Fe, Co, Ni, Cu, // 2
+    Zn, Ga, Ge, As, Se, Br, Kr, Rb, Sr, Y,  // 3
+    Zr, Nb, Mo, Tc, Ru, Rh, Pd, Ag, Cd, In, // 4
+    Sn, Sb, Te, I,  Xe, Cs, Ba, La, Ce, Pr, // 5
+    Nd, Pm, Sm, Eu, Gd, Tb, Dy, Ho, Er, Tm, // 6
+    Yb, Lu, Hf, Ta, W,  Re, Os, Ir, Pt, Au, // 7
+    Hg, Tl, Pb, Bi, Po, At, Rn, Fr, Ra, Ac, // 8
+    Th, Pa, U,  Np, Pu, Am, Cm, Bk, Cf, Es, // 9
+    Fm, Md, No, Lr, Rf, Db, Sg, Bh, Hs, Mt, // 10
+    Ds, Rg, Cn, Nh, Fl, Mc, Lv, Ts, Og      // 11
 }
 
 impl Default for Element {
