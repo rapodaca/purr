bf1701c5df409fe0038ae74e2fa4451ec23b9dd1
Richard Apodaca <rich.apodaca@gmail.com>
2021-01-29 14:02:29 -0800

    Removes unused tree/walk and tree/Follower.

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,15 +1,11 @@
 mod atom;
 mod link;
 mod target;
-mod walk;
-mod follower;
 mod from_graph;
 mod join_pool;
 
 pub use atom::Atom;
 pub use link::Link;
 pub use target::Target;
-pub use walk::walk;
-pub use follower::Follower;
 pub use from_graph::from_graph;
 pub use join_pool::JoinPool;
