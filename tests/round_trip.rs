7c1b7d3534904b1b6259b264681cc0cd1bb14a9f
Richard Apodaca <rich.apodaca@gmail.com>
2021-02-13 08:52:02 -0800

    Adds integration suite with a round-trip file-based test. Closes #11.

---
+++ tests/round_trip.rs
@@ -0,0 +1,22 @@
+use std::{ fs, path::PathBuf };
+
+use purr::read::read;
+use purr::graph::from_tree;
+use purr::tree::{ from_graph, Writer };
+
+#[test]
+fn round_trip() {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    path.push("./tests/round_trip.smi");
+
+    let file = fs::read_to_string(&path).expect("test data");
+    let entries = file.split("\n").collect::<Vec<_>>();
+
+    for entry in entries {
+        let graph = from_tree(read(entry).unwrap().root);
+        let tree = from_graph(graph).unwrap();
+
+        assert_eq!(Writer::write(&tree), entry)
+    }
+}
